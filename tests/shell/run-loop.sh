c73e482fb4092e2620220a656c4f9c9076c04981
Jeremy Sowden <azazel@debian.org>
2025-06-10 21:17:52 +0100

    New upstream version 1.1.3

--- tests/shell/run-loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while [ 1 ]
-do
-	./run-tests.sh &>> /tmp/log
-done
--- tests/shell/run-test-optimize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find testcases/ | grep dumps | grep "\.nft$" | while read line
-do
-	echo "$line ..."
-	nft -o -c -f $line
-done
